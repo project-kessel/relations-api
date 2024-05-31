merlante/insights-rebac

Initial lookup (subjects) endpoint.

api/rebac/v1/lookup_grpc.pb.go
@@ -1,0 +1,136 @@
+// Code generated by protoc-gen-go-grpc. DO NOT EDIT.
+// versions:
+// - protoc-gen-go-grpc v1.3.0
+// - protoc             v4.25.1
+// source: rebac/v1/lookup.proto
+
+package v1
+
+import (
+	context "context"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+)
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
+const _ = grpc.SupportPackageIsVersion7
+
+const (
+	Lookup_Subjects_FullMethodName = "/api.rebac.v1.Lookup/Subjects"
+)
+
+// LookupClient is the client API for Lookup service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type LookupClient interface {
+	Subjects(ctx context.Context, in *LookupSubjectsRequest, opts ...grpc.CallOption) (Lookup_SubjectsClient, error)
+}
+
+type lookupClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewLookupClient(cc grpc.ClientConnInterface) LookupClient {
+	return &lookupClient{cc}
+}
+
+func (c *lookupClient) Subjects(ctx context.Context, in *LookupSubjectsRequest, opts ...grpc.CallOption) (Lookup_SubjectsClient, error) {
+	stream, err := c.cc.NewStream(ctx, &Lookup_ServiceDesc.Streams[0], Lookup_Subjects_FullMethodName, opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &lookupSubjectsClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Lookup_SubjectsClient interface {
+	Recv() (*LookupSubjectsResponse, error)
+	grpc.ClientStream
+}
+
+type lookupSubjectsClient struct {
+	grpc.ClientStream
+}
+
+func (x *lookupSubjectsClient) Recv() (*LookupSubjectsResponse, error) {
+	m := new(LookupSubjectsResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// LookupServer is the server API for Lookup service.
+// All implementations must embed UnimplementedLookupServer
+// for forward compatibility
+type LookupServer interface {
+	Subjects(*LookupSubjectsRequest, Lookup_SubjectsServer) error
+	mustEmbedUnimplementedLookupServer()
+}
+
+// UnimplementedLookupServer must be embedded to have forward compatible implementations.
+type UnimplementedLookupServer struct {
+}
+
+func (UnimplementedLookupServer) Subjects(*LookupSubjectsRequest, Lookup_SubjectsServer) error {
+	return status.Errorf(codes.Unimplemented, "method Subjects not implemented")
+}
+func (UnimplementedLookupServer) mustEmbedUnimplementedLookupServer() {}
+
+// UnsafeLookupServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to LookupServer will
+// result in compilation errors.
+type UnsafeLookupServer interface {
+	mustEmbedUnimplementedLookupServer()
+}
+
+func RegisterLookupServer(s grpc.ServiceRegistrar, srv LookupServer) {
+	s.RegisterService(&Lookup_ServiceDesc, srv)
+}
+
+func _Lookup_Subjects_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(LookupSubjectsRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(LookupServer).Subjects(m, &lookupSubjectsServer{stream})
+}
+
+type Lookup_SubjectsServer interface {
+	Send(*LookupSubjectsResponse) error
+	grpc.ServerStream
+}
+
+type lookupSubjectsServer struct {
+	grpc.ServerStream
+}
+
+func (x *lookupSubjectsServer) Send(m *LookupSubjectsResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+// Lookup_ServiceDesc is the grpc.ServiceDesc for Lookup service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Lookup_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "api.rebac.v1.Lookup",
+	HandlerType: (*LookupServer)(nil),
+	Methods:     []grpc.MethodDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "Subjects",
+			Handler:       _Lookup_Subjects_Handler,
+			ServerStreams: true,
+		},
+	},
+	Metadata: "rebac/v1/lookup.proto",
+}

cmd/ciam-rebac/wire_gen.go
@@ -35,8 +35,9 @@ func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*
 	healthService := service.NewHealthService()
 	checkUsecase := biz.NewCheckUsecase(spiceDbRepository, logger)
 	checkService := service.NewCheckService(logger, checkUsecase)
-	grpcServer := server.NewGRPCServer(confServer, relationshipsService, healthService, checkService, logger)
-	httpServer := server.NewHTTPServer(confServer, relationshipsService, healthService, checkService, logger)
+	lookupSubjectsService := service.NewLookupSubjectsService(spiceDbRepository)
+	grpcServer := server.NewGRPCServer(confServer, relationshipsService, healthService, checkService, lookupSubjectsService, logger)
+	httpServer := server.NewHTTPServer(confServer, relationshipsService, healthService, checkService, lookupSubjectsService, logger)
 	app := newApp(logger, grpcServer, httpServer)
 	return app, func() {
 		cleanup()

internal/biz/relationships.go
@@ -12,6 +12,7 @@ type TouchSemantics bool
 
 type ZanzibarRepository interface {
 	Check(ctx context.Context, request *v1.CheckRequest) (*v1.CheckResponse, error)
+	LookupSubjects(context.Context, string, string, *v1.ObjectReference) (chan *v1.SubjectReference, chan error, error)
 	CreateRelationships(context.Context, []*v1.Relationship, TouchSemantics) error
 	ReadRelationships(context.Context, *v1.RelationshipFilter) ([]*v1.Relationship, error)
 	DeleteRelationships(context.Context, *v1.RelationshipFilter) error

internal/data/spicedb.go
@@ -7,14 +7,15 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
+	"os"
+
 	v1 "github.com/authzed/authzed-go/proto/authzed/api/v1"
 	"github.com/authzed/authzed-go/v1"
 	"github.com/authzed/grpcutil"
 	"github.com/go-kratos/kratos/v2/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"io"
-	"os"
 )
 
 // SpiceDbRepository .
@@ -73,6 +74,49 @@ func NewSpiceDbRepository(c *conf.Data, logger log.Logger) (*SpiceDbRepository, 
 	}
 
 	return &SpiceDbRepository{client}, cleanup, nil
+}
+
+func (s *SpiceDbRepository) LookupSubjects(ctx context.Context, subject_type string, relation string, object *apiV1.ObjectReference) (chan *apiV1.SubjectReference, chan error, error) {
+	client, err := s.client.LookupSubjects(ctx, &v1.LookupSubjectsRequest{
+		Resource: &v1.ObjectReference{
+			ObjectType: object.Type,
+			ObjectId:   object.Id,
+		},
+		Permission:              relation,
+		SubjectObjectType:       subject_type,
+		OptionalSubjectRelation: "",
+		OptionalConcreteLimit:   0,
+	})
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	subjects := make(chan *v1.SubjectReference)
+	errs := make(chan error, 1)
+
+	go func() {
+		msg, err := client.Recv()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				errs <- err
+			}
+			close(errs)
+			close(subjects)
+			return
+		}
+
+		subj := msg.GetSubject()
+		subjects <- &v1.SubjectReference{
+			Object: &v1.ObjectReference{
+				ObjectType: subject_type,
+				ObjectId:   subj.SubjectObjectId,
+			},
+			OptionalRelation: "",
+		}
+	}()
+
+	return nil, nil, nil
 }
 
 func (s *SpiceDbRepository) CreateRelationships(ctx context.Context, rels []*apiV1.Relationship, touch biz.TouchSemantics) error {

internal/service/lookup.go
@@ -1,0 +1,47 @@
+package service
+
+import (
+	pb "ciam-rebac/api/rebac/v1"
+	"ciam-rebac/internal/biz"
+	"context"
+)
+
+type LookupService struct {
+	pb.UnimplementedLookupServer
+	repo biz.ZanzibarRepository
+}
+
+func NewLookupSubjectsService(repo biz.ZanzibarRepository) *LookupService {
+	return &LookupService{
+		repo: repo,
+	}
+
+}
+
+func (s *LookupService) Lookup(req *pb.LookupSubjectsRequest, conn pb.Lookup_SubjectsServer) error {
+	ctx := context.TODO() //Doesn't get context from grpc?
+	subs, errs, err := s.repo.LookupSubjects(ctx, req.SubjectType, req.Relation, &pb.ObjectReference{
+		Type: req.Object.Type,
+		Id:   req.Object.Id,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	for sub := range subs {
+		err = conn.Send(&pb.LookupSubjectsResponse{
+			Subject: sub,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	err, ok := <-errs
+	if ok {
+		return err
+	}
+
+	return nil
+}

project-kessel/relations-api

api/relations/v0/lookup_grpc.pb.go
@@ -1,0 +1,136 @@
+// Code generated by protoc-gen-go-grpc. DO NOT EDIT.
+// versions:
+// - protoc-gen-go-grpc v1.3.0
+// - protoc             v4.25.1
+// source: relations/v0/lookup.proto
+
+package v0
+
+import (
+	context "context"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+)
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+// Requires gRPC-Go v1.32.0 or later.
+const _ = grpc.SupportPackageIsVersion7
+
+const (
+	Lookup_Subjects_FullMethodName = "/kessel.relations.v0.Lookup/Subjects"
+)
+
+// LookupClient is the client API for Lookup service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type LookupClient interface {
+	Subjects(ctx context.Context, in *LookupSubjectsRequest, opts ...grpc.CallOption) (Lookup_SubjectsClient, error)
+}
+
+type lookupClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewLookupClient(cc grpc.ClientConnInterface) LookupClient {
+	return &lookupClient{cc}
+}
+
+func (c *lookupClient) Subjects(ctx context.Context, in *LookupSubjectsRequest, opts ...grpc.CallOption) (Lookup_SubjectsClient, error) {
+	stream, err := c.cc.NewStream(ctx, &Lookup_ServiceDesc.Streams[0], Lookup_Subjects_FullMethodName, opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &lookupSubjectsClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type Lookup_SubjectsClient interface {
+	Recv() (*LookupSubjectsResponse, error)
+	grpc.ClientStream
+}
+
+type lookupSubjectsClient struct {
+	grpc.ClientStream
+}
+
+func (x *lookupSubjectsClient) Recv() (*LookupSubjectsResponse, error) {
+	m := new(LookupSubjectsResponse)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// LookupServer is the server API for Lookup service.
+// All implementations must embed UnimplementedLookupServer
+// for forward compatibility
+type LookupServer interface {
+	Subjects(*LookupSubjectsRequest, Lookup_SubjectsServer) error
+	mustEmbedUnimplementedLookupServer()
+}
+
+// UnimplementedLookupServer must be embedded to have forward compatible implementations.
+type UnimplementedLookupServer struct {
+}
+
+func (UnimplementedLookupServer) Subjects(*LookupSubjectsRequest, Lookup_SubjectsServer) error {
+	return status.Errorf(codes.Unimplemented, "method Subjects not implemented")
+}
+func (UnimplementedLookupServer) mustEmbedUnimplementedLookupServer() {}
+
+// UnsafeLookupServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to LookupServer will
+// result in compilation errors.
+type UnsafeLookupServer interface {
+	mustEmbedUnimplementedLookupServer()
+}
+
+func RegisterLookupServer(s grpc.ServiceRegistrar, srv LookupServer) {
+	s.RegisterService(&Lookup_ServiceDesc, srv)
+}
+
+func _Lookup_Subjects_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(LookupSubjectsRequest)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(LookupServer).Subjects(m, &lookupSubjectsServer{stream})
+}
+
+type Lookup_SubjectsServer interface {
+	Send(*LookupSubjectsResponse) error
+	grpc.ServerStream
+}
+
+type lookupSubjectsServer struct {
+	grpc.ServerStream
+}
+
+func (x *lookupSubjectsServer) Send(m *LookupSubjectsResponse) error {
+	return x.ServerStream.SendMsg(m)
+}
+
+// Lookup_ServiceDesc is the grpc.ServiceDesc for Lookup service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var Lookup_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "kessel.relations.v0.Lookup",
+	HandlerType: (*LookupServer)(nil),
+	Methods:     []grpc.MethodDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "Subjects",
+			Handler:       _Lookup_Subjects_Handler,
+			ServerStreams: true,
+		},
+	},
+	Metadata: "relations/v0/lookup.proto",
+}

cmd/ciam-rebac/wire_gen.go
@@ -35,8 +35,9 @@ func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*
 	healthService := service.NewHealthService()
 	checkUsecase := biz.NewCheckUsecase(spiceDbRepository, logger)
 	checkService := service.NewCheckService(logger, checkUsecase)
-	grpcServer := server.NewGRPCServer(confServer, relationshipsService, healthService, checkService, logger)
-	httpServer := server.NewHTTPServer(confServer, relationshipsService, healthService, checkService, logger)
+	lookupService := service.NewLookupSubjectsService(spiceDbRepository)
+	grpcServer := server.NewGRPCServer(confServer, relationshipsService, healthService, checkService, lookupService, logger)
+	httpServer := server.NewHTTPServer(confServer, relationshipsService, healthService, checkService, lookupService, logger)
 	app := newApp(logger, grpcServer, httpServer)
 	return app, func() {
 		cleanup()

internal/biz/relationships.go
@@ -15,6 +15,7 @@ type ZanzibarRepository interface {
 	CreateRelationships(context.Context, []*v0.Relationship, TouchSemantics) error
 	ReadRelationships(context.Context, *v0.RelationTupleFilter) ([]*v0.Relationship, error)
 	DeleteRelationships(context.Context, *v0.RelationTupleFilter) error
+	LookupSubjects(context.Context, string, string, *v0.ObjectReference) (chan *v0.SubjectReference, chan error, error)
 }
 
 type CheckUsecase struct {

internal/data/spicedb.go
@@ -77,6 +77,49 @@ func NewSpiceDbRepository(c *conf.Data, logger log.Logger) (*SpiceDbRepository, 
 	return &SpiceDbRepository{client}, cleanup, nil
 }
 
+func (s *SpiceDbRepository) LookupSubjects(ctx context.Context, subject_type string, relation string, object *apiV0.ObjectReference) (chan *apiV0.SubjectReference, chan error, error) {
+	client, err := s.client.LookupSubjects(ctx, &v1.LookupSubjectsRequest{
+		Resource: &v1.ObjectReference{
+			ObjectType: kesselTypeToSpiceDBType(object.Type),
+			ObjectId:   object.Id,
+		},
+		Permission:              relation,
+		SubjectObjectType:       subject_type,
+		OptionalSubjectRelation: "",
+		OptionalConcreteLimit:   0,
+	})
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	subjects := make(chan *v1.SubjectReference)
+	errs := make(chan error, 1)
+
+	go func() {
+		msg, err := client.Recv()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				errs <- err
+			}
+			close(errs)
+			close(subjects)
+			return
+		}
+
+		subj := msg.GetSubject()
+		subjects <- &v1.SubjectReference{
+			Object: &v1.ObjectReference{
+				ObjectType: subject_type,
+				ObjectId:   subj.SubjectObjectId,
+			},
+			OptionalRelation: "",
+		}
+	}()
+
+	return nil, nil, nil
+}
+
 func (s *SpiceDbRepository) CreateRelationships(ctx context.Context, rels []*apiV0.Relationship, touch biz.TouchSemantics) error {
 	var relationshipUpdates []*v1.RelationshipUpdate
 

internal/service/lookup.go
@@ -1,0 +1,47 @@
+package service
+
+import (
+	pb "ciam-rebac/api/relations/v0"
+	"ciam-rebac/internal/biz"
+	"context"
+)
+
+type LookupService struct {
+	pb.UnimplementedLookupServer
+	repo biz.ZanzibarRepository
+}
+
+func NewLookupSubjectsService(repo biz.ZanzibarRepository) *LookupService {
+	return &LookupService{
+		repo: repo,
+	}
+
+}
+
+func (s *LookupService) Lookup(req *pb.LookupSubjectsRequest, conn pb.Lookup_SubjectsServer) error {
+	ctx := context.TODO() //Doesn't get context from grpc?
+	subs, errs, err := s.repo.LookupSubjects(ctx, req.SubjectType, req.Relation, &pb.ObjectReference{
+		Type: req.Object.Type,
+		Id:   req.Object.Id,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	for sub := range subs {
+		err = conn.Send(&pb.LookupSubjectsResponse{
+			Subject: sub,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	err, ok := <-errs
+	if ok {
+		return err
+	}
+
+	return nil
+}
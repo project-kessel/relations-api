60aa58cde7564116051f9b2a58910214fb810bf4
Mark McLaughlin <mmclaugh@redhat.com>
2024-06-13 14:28:38 +0100

    Remove superfluous and obsolete -compose check.

--- spicedb/check_docker_podman.sh
+++ spicedb/check_docker_podman.sh
@@ -18,19 +18,5 @@ else
     echo "Podman not found. Please install either Docker or Podman to proceed."
     exit 1
   fi
-
-   # Check if docker-compose is installed
-    if command_exists docker-compose; then
-      echo "docker-compose is installed."
-    else
-
-      # Check if podman-compose is installed
-      if command_exists podman-compose; then
-        echo "podman-compose is installed."
-      else
-        echo "docker-compose or podman-compose not found. Please install either docker-compose or podman-compose to proceed."
-        exit 1
-      fi
-    fi
 fi
 

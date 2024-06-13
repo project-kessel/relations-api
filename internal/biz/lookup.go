b1b36c7ceac1819d301bf550bbcc6099d7ee54ae
Jonathan Marcantonio <jmarcant@redhat.com>
2024-06-13 16:03:25 -0400

    Validate LookupSubjectsRequest request body
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- internal/biz/lookup.go
+++ internal/biz/lookup.go
@@ -37,11 +37,17 @@ func (s *GetSubjectsUsecase) Get(ctx context.Context, req *v0.LookupSubjectsRequ
 	}
 
 	if req.Resource == nil {
+		s.log.WithContext(ctx).Infof("Missing Resource in request %v", req)
 		return nil, nil, errors.BadRequest("Invalid request", "Object is required")
 	}
 
 	if req.SubjectRelation != nil {
 		subjectRelation = *req.SubjectRelation
+	}
+
+	if req.SubjectType == nil {
+		s.log.WithContext(ctx).Infof("Missing SubjectType in request %v", req)
+		return nil, nil, errors.BadRequest("Invalid request", "Subject type is required")
 	}
 
 	subs, errs, err := s.repo.LookupSubjects(ctx, req.SubjectType, subjectRelation, req.Relation, &v0.ObjectReference{

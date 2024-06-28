5f7fbf8644a61aa71f1e78566a7b87dcecb121c8
Jonathan Marcantonio <jmarcant@redhat.com>
2024-06-28 08:01:26 -0400

    Add request validation for check requests (#114)
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- internal/service/check.go
+++ internal/service/check.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/project-kessel/relations-api/internal/biz"
 
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
 
 	pb "github.com/project-kessel/relations-api/api/relations/v0"
@@ -25,6 +26,21 @@ func NewCheckService(logger log.Logger, checkUseCase *biz.CheckUsecase) *CheckSe
 }
 
 func (s *CheckService) Check(ctx context.Context, req *pb.CheckRequest) (*pb.CheckResponse, error) {
+	if err := req.ValidateAll(); err != nil {
+		s.log.Infof("Request failed to pass validation: %v", req)
+		return nil, errors.BadRequest("Invalid request", err.Error())
+	}
+
+	if err := req.Subject.ValidateAll(); err != nil {
+		s.log.Infof("Subject failed to pass validation: %v", req)
+		return nil, errors.BadRequest("Invalid request", err.Error())
+	}
+
+	if err := req.Resource.ValidateAll(); err != nil {
+		s.log.Infof("Resource failed to pass validation: %v", req)
+		return nil, errors.BadRequest("Invalid request", err.Error())
+	}
+
 	resp, err := s.check.Check(ctx, req)
 	if err != nil {
 		return resp, fmt.Errorf("failed to perform check: %w", err)

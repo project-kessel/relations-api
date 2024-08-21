126d884c61e173f94b4cd14df200b8123360e415
Jonathan Marcantonio <jmarcant@redhat.com>
2024-08-21 15:18:22 -0400

    Test streaming validation interceptor
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- internal/server/middleware/validation_test.go
+++ internal/server/middleware/validation_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/bufbuild/protovalidate-go"
 	"github.com/project-kessel/relations-api/api/kessel/relations/v1beta1"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 )
 
 func TestValidationMiddleware_ValidRequest(t *testing.T) {
@@ -24,7 +25,8 @@ func TestValidationMiddleware_ValidRequest(t *testing.T) {
 	resp, err := m(handler)(context.Background(), &v1beta1.LookupSubjectsRequest{
 		Resource: &v1beta1.ObjectReference{
 			Type: &v1beta1.ObjectType{Namespace: "rbac", Name: "user"},
-			Id:   "bob"},
+			Id:   "bob",
+		},
 		Relation:    "member",
 		SubjectType: &v1beta1.ObjectType{Namespace: "rbac", Name: "group"},
 	})
@@ -49,4 +51,74 @@ func TestValidationMiddleware_InvalidRequest(t *testing.T) {
 	})
 	assert.Error(t, err)
 	assert.Equal(t, resp, nil)
+}
+
+type DummyServerStream struct {
+	grpc.ServerStream
+	RecvMsgFunc func(msg interface{}) error
+}
+
+func (m *DummyServerStream) RecvMsg(msg interface{}) error {
+	return m.RecvMsgFunc(msg)
+}
+
+func TestStreamValidationInterceptor_ValidRequest(t *testing.T) {
+	t.Parallel()
+
+	validator, err := protovalidate.New()
+	assert.NoError(t, err)
+
+	interceptor := StreamValidationInterceptor(validator)
+
+	dummyStream := &DummyServerStream{
+		RecvMsgFunc: func(msg interface{}) error {
+			*msg.(*v1beta1.LookupSubjectsRequest) = v1beta1.LookupSubjectsRequest{
+				Resource: &v1beta1.ObjectReference{
+					Type: &v1beta1.ObjectType{Namespace: "rbac", Name: "user"},
+					Id:   "bob",
+				},
+				Relation:    "member",
+				SubjectType: &v1beta1.ObjectType{Namespace: "rbac", Name: "group"},
+			}
+			return nil
+		},
+	}
+
+	handler := func(srv interface{}, stream grpc.ServerStream) error {
+		msg := &v1beta1.LookupSubjectsRequest{}
+		err := stream.RecvMsg(msg)
+		assert.NoError(t, err)
+		return nil
+	}
+
+	err = interceptor(nil, dummyStream, nil, handler)
+	assert.NoError(t, err)
+}
+
+func TestStreamValidationInterceptor_InvalidRequest(t *testing.T) {
+	t.Parallel()
+
+	validator, err := protovalidate.New()
+	assert.NoError(t, err)
+
+	interceptor := StreamValidationInterceptor(validator)
+
+	dummyStream := &DummyServerStream{
+		RecvMsgFunc: func(msg interface{}) error {
+			*msg.(*v1beta1.LookupSubjectsRequest) = v1beta1.LookupSubjectsRequest{
+				Resource: &v1beta1.ObjectReference{},
+			}
+			return nil
+		},
+	}
+
+	handler := func(srv interface{}, stream grpc.ServerStream) error {
+		msg := &v1beta1.LookupSubjectsRequest{}
+		err := stream.RecvMsg(msg)
+		assert.Error(t, err)
+		return err
+	}
+
+	err = interceptor(nil, dummyStream, nil, handler)
+	assert.Error(t, err)
 }

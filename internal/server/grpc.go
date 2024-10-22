edeff962dcc9582bf9cd55dac472280ff0169a40
wscalf <wscalf@gmail.com>
2024-10-22 16:57:22 -0400

    Reorganize middleware so enabling auth doesn't overwrite others Add a test that ensures validation still works with auth enabled

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -18,6 +18,7 @@ import (
 
 	"go.opentelemetry.io/otel/metric"
 
+	kratosmiddleware "github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	kesselMetrics "github.com/project-kessel/relations-api/internal/server/middleware/metrics"
 	kesselRecovery "github.com/project-kessel/relations-api/internal/server/middleware/recovery"
@@ -38,41 +39,50 @@ func NewGRPCServer(c *conf.Server, relations *service.RelationshipsService, heal
 	if err != nil {
 		return nil, err
 	}
-	var opts = []grpc.ServerOption{
-		grpc.Middleware(
-			recovery.Recovery(),
-			middleware.ValidationMiddleware(validator),
-			logging.Server(logger),
-			metrics.Server(
-				metrics.WithSeconds(seconds),
-				metrics.WithRequests(requests),
-			),
+
+	unaryMiddleware := []kratosmiddleware.Middleware{
+		recovery.Recovery(),
+		middleware.ValidationMiddleware(validator),
+		logging.Server(logger),
+		metrics.Server(
+			metrics.WithSeconds(seconds),
+			metrics.WithRequests(requests),
 		),
-		grpc.Options(googlegrpc.ChainStreamInterceptor(
-			middleware.StreamLogInterceptor(logger),
-			middleware.StreamValidationInterceptor(validator),
-			kesselRecovery.StreamRecoveryInterceptor(logger),
-			kesselMetrics.StreamMetricsInterceptor(
-				kesselMetrics.WithSeconds(seconds),
-				kesselMetrics.WithRequests(requests),
-			),
-		)),
 	}
+	streamingMiddleware := []googlegrpc.StreamServerInterceptor{
+		middleware.StreamLogInterceptor(logger),
+		middleware.StreamValidationInterceptor(validator),
+		kesselRecovery.StreamRecoveryInterceptor(logger),
+		kesselMetrics.StreamMetricsInterceptor(
+			kesselMetrics.WithSeconds(seconds),
+			kesselMetrics.WithRequests(requests),
+		),
+	}
+
 	if c.Auth.EnableAuth {
 		jwks, err := FetchJwks(c.Auth.JwksUrl)
 		if err != nil {
 			return nil, err
 		}
-		opts = append(opts, grpc.Middleware(
+
+		unaryMiddleware = append(unaryMiddleware,
 			selector.Server(jwt.Server(jwks.Keyfunc,
 				jwt.WithSigningMethod(jwtv5.SigningMethodRS256))).
 				Match(NewWhiteListMatcher).
 				Build(),
-		),
-			grpc.Options(googlegrpc.ChainStreamInterceptor(auth.StreamAuthInterceptor(
-				jwks.Keyfunc,
-				auth.WithSigningMethod(jwtv5.SigningMethodRS256)))),
 		)
+		streamingMiddleware = append(streamingMiddleware, auth.StreamAuthInterceptor(
+			jwks.Keyfunc,
+			auth.WithSigningMethod(jwtv5.SigningMethodRS256)))
+	}
+
+	var opts = []grpc.ServerOption{
+		grpc.Middleware(
+			unaryMiddleware...,
+		),
+		grpc.Options(googlegrpc.ChainStreamInterceptor(
+			streamingMiddleware...,
+		)),
 	}
 	if c.Grpc.Network != "" {
 		opts = append(opts, grpc.Network(c.Grpc.Network))
@@ -83,6 +93,7 @@ func NewGRPCServer(c *conf.Server, relations *service.RelationshipsService, heal
 	if c.Grpc.Timeout != nil {
 		opts = append(opts, grpc.Timeout(c.Grpc.Timeout.AsDuration()))
 	}
+
 	srv := grpc.NewServer(opts...)
 	v1beta1.RegisterKesselTupleServiceServer(srv, relations)
 	v1beta1.RegisterKesselCheckServiceServer(srv, check)

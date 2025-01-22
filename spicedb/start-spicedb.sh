6d8117146c705c8f0fa978a4db54cf7db3ae5f66
Antony Natale <43508092+tonytheleg@users.noreply.github.com>
2025-01-22 13:18:51 -0500

    updates compose files to better support integration with inventory (#342)

--- spicedb/start-spicedb.sh
+++ spicedb/start-spicedb.sh
@@ -2,4 +2,6 @@
 set -e
 # Function to check if a command is available
 source ./spicedb/check_docker_podman.sh
+NETWORK_CHECK=$(${DOCKER} network ls --filter name=kessel --format json)
+if [[ -z "${NETWORK_CHECK}" ]]; then ${DOCKER} network create kessel; fi
 ${DOCKER} compose --env-file ./spicedb/.env -f ./docker-compose.yaml up -d

91c9361216cc8590a22fc0342ded198c25292d8c
Jonathan Marcantonio <jmarcant@redhat.com>
2025-01-22 14:56:41 -0500

    Update network check compatibility with podman
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- spicedb/start-spicedb.sh
+++ spicedb/start-spicedb.sh
@@ -3,5 +3,5 @@ set -e
 # Function to check if a command is available
 source ./spicedb/check_docker_podman.sh
 NETWORK_CHECK=$(${DOCKER} network ls --filter name=kessel --format json)
-if [[ -z "${NETWORK_CHECK}" ]]; then ${DOCKER} network create kessel; fi
+if [[ -z "${NETWORK_CHECK}" || "${NETWORK_CHECK}" == "[]" ]]; then ${DOCKER} network create kessel; fi
 ${DOCKER} compose --env-file ./spicedb/.env -f ./docker-compose.yaml up -d

6b3c464e12929f374e089ed746b73cc90d645dc2
Jonathan Marcantonio <jmarcant@redhat.com>
2025-01-24 12:37:42 -0500

    Download latest prod schema before running (#346)
    * Download latest prod schema before running
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Update readme to mention pulling schema
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    ---------
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- spicedb/start-relations-api.sh
+++ spicedb/start-relations-api.sh
@@ -4,4 +4,6 @@ set -e
 source ./spicedb/check_docker_podman.sh
 NETWORK_CHECK=$(${DOCKER} network ls --filter name=kessel --format json)
 if [[ -z "${NETWORK_CHECK}" || "${NETWORK_CHECK}" == "[]" ]]; then ${DOCKER} network create kessel; fi
+echo "Downloading latest schema"
+curl -o deploy/schema.zed https://raw.githubusercontent.com/RedHatInsights/rbac-config/refs/heads/master/configs/prod/schemas/schema.zed
 ${DOCKER} compose --env-file ./spicedb/.env --profile relations-api -f ./docker-compose.yaml up -d --build
